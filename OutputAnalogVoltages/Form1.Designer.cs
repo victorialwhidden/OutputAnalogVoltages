36470ffac3265c015dc3a228919c225967741513
whiddenv <whiddenv@PHY-GWH244-04>
2025-02-23 16:34:13 -0500

    Add project files.

---
+++ OutputAnalogVoltages/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace OutputAnalogVoltages
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_labname = new System.Windows.Forms.Label();
+            this.Cbx_AOports = new System.Windows.Forms.ComboBox();
+            this.HscB_AnalogV = new System.Windows.Forms.HScrollBar();
+            this.txtB_AOVoltage = new System.Windows.Forms.TextBox();
+            this.lstB_DevVoltages = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbl_labname
+            // 
+            this.lbl_labname.AutoSize = true;
+            this.lbl_labname.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_labname.Location = new System.Drawing.Point(12, 25);
+            this.lbl_labname.Name = "lbl_labname";
+            this.lbl_labname.Size = new System.Drawing.Size(204, 23);
+            this.lbl_labname.TabIndex = 0;
+            this.lbl_labname.Text = "Lab: Analog Outputs";
+            // 
+            // Cbx_AOports
+            // 
+            this.Cbx_AOports.FormattingEnabled = true;
+            this.Cbx_AOports.Location = new System.Drawing.Point(116, 135);
+            this.Cbx_AOports.Name = "Cbx_AOports";
+            this.Cbx_AOports.Size = new System.Drawing.Size(237, 21);
+            this.Cbx_AOports.TabIndex = 1;
+            this.Cbx_AOports.SelectedIndexChanged += new System.EventHandler(this.Cbx_AOports_SelectedIndexChanged);
+            // 
+            // HscB_AnalogV
+            // 
+            this.HscB_AnalogV.Location = new System.Drawing.Point(66, 195);
+            this.HscB_AnalogV.Name = "HscB_AnalogV";
+            this.HscB_AnalogV.Size = new System.Drawing.Size(336, 27);
+            this.HscB_AnalogV.TabIndex = 2;
+            this.HscB_AnalogV.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HscB_AnalogV_Scroll);
+            // 
+            // txtB_AOVoltage
+            // 
+            this.txtB_AOVoltage.BackColor = System.Drawing.SystemColors.Info;
+            this.txtB_AOVoltage.Location = new System.Drawing.Point(150, 162);
+            this.txtB_AOVoltage.Name = "txtB_AOVoltage";
+            this.txtB_AOVoltage.ReadOnly = true;
+            this.txtB_AOVoltage.Size = new System.Drawing.Size(168, 20);
+            this.txtB_AOVoltage.TabIndex = 3;
+            // 
+            // lstB_DevVoltages
+            // 
+            this.lstB_DevVoltages.Enabled = false;
+            this.lstB_DevVoltages.FormattingEnabled = true;
+            this.lstB_DevVoltages.Location = new System.Drawing.Point(500, 64);
+            this.lstB_DevVoltages.Name = "lstB_DevVoltages";
+            this.lstB_DevVoltages.Size = new System.Drawing.Size(254, 251);
+            this.lstB_DevVoltages.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstB_DevVoltages);
+            this.Controls.Add(this.txtB_AOVoltage);
+            this.Controls.Add(this.HscB_AnalogV);
+            this.Controls.Add(this.Cbx_AOports);
+            this.Controls.Add(this.lbl_labname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_labname;
+        private System.Windows.Forms.ComboBox Cbx_AOports;
+        private System.Windows.Forms.HScrollBar HscB_AnalogV;
+        private System.Windows.Forms.TextBox txtB_AOVoltage;
+        private System.Windows.Forms.ListBox lstB_DevVoltages;
+    }
+}
+
---
+++ OutputAnalogVoltages/Form1.cs
@@ -0,0 +1,123 @@
+﻿using NationalInstruments.DAQmx;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace OutputAnalogVoltages
+{
+    public partial class Form1 : Form
+    {   double AnalogV;
+        NationalInstruments.DAQmx.Task analogOutTask = new NationalInstruments.DAQmx.Task(); //Creating a global task
+        AnalogMultiChannelWriter writer;
+
+        List<double> voltageslist = new List<double>(); //creating a new list (similar to an array but it is flexible)
+        string[] ao_Channels;
+        public Form1()
+        {
+            InitializeComponent();
+
+            voltageslist.Add(0);
+            voltageslist.Add(0);
+
+            //Initializing the scroll bar
+            HscB_AnalogV.Minimum = -1000;
+            HscB_AnalogV.Maximum = 1000 + HscB_AnalogV.LargeChange - 1; //bug when using scroll bar
+            HscB_AnalogV.LargeChange = 10;
+            HscB_AnalogV.SmallChange = 1;
+            HscB_AnalogV.Value = 0;
+
+            //Allowing 0V to be the default
+            AnalogV = HscB_AnalogV.Value / 100.00;
+            txtB_AOVoltage.Text = AnalogV.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            writer = new AnalogMultiChannelWriter(analogOutTask.Stream);
+
+            try
+            {
+
+                ao_Channels = DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AO, PhysicalChannelAccess.External);
+                //voltageslist = new List<double>();
+                Cbx_AOports.DropDownStyle = ComboBoxStyle.DropDownList; //Non-editable combobox
+                Cbx_AOports.Items.AddRange(ao_Channels); //Adds channels to combobox
+                lstB_DevVoltages.Items.Clear(); //Want to clear the list box before adding anything
+
+                for (int i = 0; i < ao_Channels.Length; i++)
+                {
+                    lstB_DevVoltages.Items.Add(string.Format("{0}  {1} V", ao_Channels[i], AnalogV)); //ATTEMPTING to add DEV/Channel and Voltage to listbox
+                }
+
+                if (analogOutTask != null)
+                {
+                    for (int i = 0; i < Cbx_AOports.Items.Count; i++) // Loop through ports
+                    {
+                        string deviceChannels = Cbx_AOports.Items[i].ToString();
+                        analogOutTask.AOChannels.CreateVoltageChannel(deviceChannels, "", -10, 10, AOVoltageUnits.Volts);
+                        //create a V channel for each port
+                    }
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show("There was an issue getting the channels in the daq", ex.Message); }
+            try
+            {
+                if (Cbx_AOports.Items.Count > 0)
+                {
+                    Cbx_AOports.SelectedIndex = 0;
+                }
+                else
+                {
+                    MessageBox.Show("DAQ is not turned on"); //Makes sure DAQ is on
+                    this.Close(); //Closes the program
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show("There was an error in selecting the index", ex.Message); }
+
+        }
+
+        private void HscB_AnalogV_Scroll(object sender, ScrollEventArgs e)
+        {      
+            double AnalogV = HscB_AnalogV.Value / 100.00;
+            txtB_AOVoltage.Text = AnalogV.ToString();
+            int selectedIndex = Cbx_AOports.SelectedIndex;
+
+
+            if (selectedIndex == 0)
+            {
+                voltageslist[selectedIndex] = AnalogV;
+                lstB_DevVoltages.Items[0] = (string.Format("{0}  {1} V", ao_Channels[selectedIndex], AnalogV));
+            }
+            else
+            {
+                voltageslist[selectedIndex] = AnalogV;
+                lstB_DevVoltages.Items.RemoveAt(selectedIndex);
+                lstB_DevVoltages.Items.Add(string.Format("{0}  {1} V", ao_Channels[selectedIndex], AnalogV));
+            }
+
+                writer.WriteSingleSample(true, voltageslist.ToArray());         
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            if (analogOutTask != null)
+            {
+                double[] zero_arr = { 0, 0 };
+                writer.WriteSingleSample(true, zero_arr);
+                analogOutTask.Dispose(); 
+            //Do not put application Exit !!!! Will go into the form closing
+            }
+        }
+
+        private void Cbx_AOports_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = Cbx_AOports.SelectedIndex;
+            txtB_AOVoltage.Text = voltageslist[selectedIndex].ToString();
+            HscB_AnalogV.Value = (int)voltageslist[selectedIndex];
+        }
+    }
+}
